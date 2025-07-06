cd66c5e27d53defe84e4cc3e6c829868732b1270
Sagnik Mandal <acriticalcynic@outlook.com>
2025-07-06 13:41:10 +0530

    Refactor Config

---
+++ src/app/config/helm.ts
@@ -0,0 +1,164 @@
+interface TeamMember {
+  name: string;
+  email: string;
+  por: string;
+  photo?: string;
+  socials: {
+    twitter?: string;
+    linkedin?: string;
+    github?: string;
+    instagram?: string;
+    website?: string;
+  };
+}
+
+interface HelmTerm {
+  term: string;
+  team: TeamMember[];
+}
+
+const helm: HelmTerm[] = [
+  {
+    term: '2025-2026',
+    team: [
+      {
+        name: 'Sagnik Mandal',
+        email: 'sagnik.mandal.mst23@iitbhu.ac.in',
+        por: 'Secretary',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+      {
+        name: 'Pratham Seth',
+        email: 'pratham.seth.cer23@itbhu.ac.in',
+        por: 'Joint Secretary',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+      {
+        name: 'Sakshi Kumar',
+        email: 'sakshi.kumar.cd.eee23@itbhu.ac.in',
+        por: 'Joint Secretary',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+      {
+        name: 'Aayush Khanna',
+        email: 'aayush.khanna.cd.civ23@iitbhu.ac.in',
+        por: 'Technical Lead',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+      {
+        name: 'Parth Dabas',
+        email: 'parth.dabas.cse23@iitbhu.ac.in',
+        por: 'Technical Lead',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+      {
+        name: 'Yashashwi Singhania',
+        email: 'yashashwi.singhania.bce23@itbhu.ac.in',
+        por: 'Category Head',
+        photo: 'https://i.pravatar.cc/300',
+        socials: {},
+      },
+    ],
+  },
+  {
+    term: '2024-2025',
+    team: [
+      {
+        name: 'Shivansh Bhatnagar',
+        email: '',
+        por: 'Secretary',
+        socials: {},
+      },
+      {
+        name: 'Yuval Goyal',
+        email: '',
+        por: 'Joint Secretary',
+        socials: {},
+      },
+      {
+        name: 'Nikhil Totla',
+        email: '',
+        por: 'Joint Secretary',
+        socials: {},
+      },
+      {
+        name: 'Varun Kolanu',
+        email: '',
+        por: 'Software Development Group Head',
+        socials: {},
+      },
+      {
+        name: 'Jay Vinchhi',
+        email: '',
+        por: 'Competitive Programming Group Head',
+        socials: {},
+      },
+      {
+        name: 'Shashank Shekhar Singh',
+        email: '',
+        por: 'Intelligence Group Head',
+        socials: {},
+      },
+      {
+        name: 'Abhinav Lenka',
+        email: '',
+        por: 'Infosec Head',
+        socials: {},
+      },
+    ],
+  },
+  {
+    term: '2023-2024',
+    team: [
+      {
+        name: 'Eshaan Aggarwal',
+        email: '',
+        por: 'Secretary',
+        socials: {},
+      },
+      {
+        name: 'Anshiv Singla',
+        email: '',
+        por: 'Joint Secretary',
+        socials: {},
+      },
+      {
+        name: 'Aakash Sharma',
+        email: '',
+        por: 'Joint Secretary',
+        socials: {},
+      },
+      {
+        name: 'Vaibhav Tripathi',
+        email: '',
+        por: 'Software Development Group Head',
+        socials: {},
+      },
+      {
+        name: 'Vaibhav Khater',
+        email: '',
+        por: 'Competitive Programming Group Head',
+        socials: {},
+      },
+      {
+        name: 'Dhruvi Jain',
+        email: '',
+        por: 'Intelligence Group Head',
+        socials: {},
+      },
+      {
+        name: 'Yash Somalkar',
+        email: '',
+        por: 'Infosec Head',
+        socials: {},
+      },
+    ],
+  },
+];
+
+export { helm };
